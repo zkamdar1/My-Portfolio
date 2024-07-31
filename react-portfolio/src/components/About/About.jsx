fb8c88d6f1c1f6bed781c7612ae96d202d0c9ed5
zkamdar <zkamdar0908@gmail.com>
2024-07-30 23:17:09 -0400

    Finished the about section

---
+++ react-portfolio/src/components/About/About.jsx
@@ -0,0 +1,50 @@
+import React from 'react'
+import { getImageUrl } from '../../utils'
+import styles from './About.module.css'
+
+export const About = () => {
+  return (
+    <section className={styles.container} id='about'>
+        <h2 className={styles.title}>About</h2>
+        <div className={styles.content}>
+            <img 
+                src={getImageUrl("about/aboutImage.png")}
+                alt='Me sitting with arms crossed'
+                className={styles.aboutImage}
+            />
+            <ul className={styles.aboutItems}>
+                <li className={styles.aboutItem}>
+                    <img
+                        src={getImageUrl("about/cursorIcon.png")}
+                        alt='Cursor'
+                    />
+                    <div className={styles.aboutItemText}>
+                        <h3>Frontend Developer</h3>
+                        <p>Frontend Dev with experience in blah blah</p>
+                    </div>
+                </li>
+                <li className={styles.aboutItem}>
+                    <img
+                        src={getImageUrl("about/serverIcon.png")}
+                        alt='Server'
+                    />
+                    <div className={styles.aboutItemText}>
+                        <h3>Backend Developer</h3>
+                        <p>Backend Dev with experience in blah blah</p>
+                    </div>
+                </li>
+                <li className={styles.aboutItem}>
+                    <img
+                        src={getImageUrl("about/uiIcon.png")}
+                        alt='ui Icon'
+                    />
+                    <div className={styles.aboutItemText}>
+                        <h3>UI/UX Designer</h3>
+                        <p>UI and UX Designer with experience in blah blah</p>
+                    </div>
+                </li>
+            </ul>
+        </div>
+    </section>
+  );
+};
