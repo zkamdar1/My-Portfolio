b23576d212aeea28cef58fc4c5fe4b5e28ee7b40
zkamdar <zkamdar0908@gmail.com>
2024-08-01 11:49:38 -0400

    Completed projects section

--- react-portfolio/src/components/Projects/ProjectCard.jsx
+++ react-portfolio/src/components/Projects/ProjectCard.jsx
@@ -1,7 +1,28 @@
 import React from 'react'
+import { getImageUrl } from '../../utils';
+import styles from './ProjectCard.module.css';
 
-export const ProjectCard = () => {
+export const ProjectCard = ({ project: {title, imageSrc, description, skills, demo, source }}) => {
   return (
-    <div>ProjectCard</div>
-  )
-}
+    <div className={styles.container}>
+        <img 
+            src={getImageUrl(imageSrc)} 
+            alt={`Image of ${title}`} 
+            className={styles.image}
+        />
+        <h3 className={styles.title}>{title}</h3>
+        <p className={styles.description}>{description}</p>
+        <ul className={styles.skills}>
+            {skills.map((skill, id) => {
+                return (
+                    <li key={id} className={styles.skill}>{skill}</li>
+                );
+            })}
+        </ul>
+        <div className={styles.links}>
+            <a href={demo} className={styles.link}>Demo</a>
+            <a href={source} className={styles.link}>Source</a>
+        </div>
+    </div>
+  );
+};
--- react-portfolio/src/components/Projects/Projects.jsx
+++ react-portfolio/src/components/Projects/Projects.jsx
@@ -1,32 +1,17 @@
 import React from 'react';
 import projects from '../../data/projects.json';
-import { getImageUrl } from '../../utils'
+import styles from './Projects.module.css';
+import { ProjectCard } from './ProjectCard';
 
 export const Projects = () => {
   return (
-    <section>
-        <h2>Projects</h2>
-        <div>
+    <section className={styles.container} id='projects'>
+        <h2 className={styles.title}>Projects</h2>
+        <div className={styles.projects}>
             {
                 projects.map((project, id) => {
                     return (
-                        <div key={id}>
-                            <img 
-                                src={getImageUrl(project.imageSrc)} 
-                                alt={`Image of ${project.title}`} 
-                            />
-                            <h3>{project.title}</h3>
-                            <p>{project.description}</p>
-                            <ul>
-                                {project.skills.map((skill, id) => {
-                                <li key={id}>{skill}</li>;
-                                })}
-                            </ul>
-                            <div>
-                                <a href={project.demo}>Demo</a>
-                                <a href={project.source}>Source</a>
-                            </div>
-                        </div>
+                        <ProjectCard key={id} project={project} />
                     );
                 })
             }
