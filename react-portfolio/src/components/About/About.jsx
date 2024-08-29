4de2490f85e930ce66f5c8fe5fdb558978b85067
zkamdar <zkamdar0908@gmail.com>
2024-08-29 14:59:13 -0400

    Update portfolio information

--- react-portfolio/src/components/About/About.jsx
+++ react-portfolio/src/components/About/About.jsx
@@ -19,8 +19,8 @@ export const About = () => {
                         alt='Cursor'
                     />
                     <div className={styles.aboutItemText}>
-                        <h3>Aspiring Full-Stack Developer</h3>
-                        <p>As a Computer Science student at the University of North Carolina at Chapel Hill, I thrive on learning new technologies and tackling challenging problems.</p>
+                        <h3> Software Engineer</h3>
+                        <p>Enthusiastic Software Engineer with a background in Computer Science from the University of North Carolina at Chapel Hill. Passionate about building innovative solutions and continuously expanding my expertise in full-stack development and new technologies.</p>
                     </div>
                 </li>
                 <li className={styles.aboutItem}>
@@ -30,7 +30,7 @@ export const About = () => {
                     />
                     <div className={styles.aboutItemText}>
                         <h3>Technical Skills</h3>
-                        <p>Proficient in JavaScript, Python, React, and Node.js. Experienced in various programming languages and frameworks, including web development and software engineering practices.</p>
+                        <p>Skilled in using a wide range of programming languages and frameworks to build innovative solutions. Experienced in full-stack development, optimizing performance, and enhancing user experiences. Familiar with software engineering best practices, version control systems, and Agile development methodologies.</p>
                     </div>
                 </li>
                 <li className={styles.aboutItem}>
@@ -39,8 +39,8 @@ export const About = () => {
                         alt='ui Icon'
                     />
                     <div className={styles.aboutItemText}>
-                        <h3>Team Player</h3>
-                        <p>Strong collaborator with excellent communication skills, committed to working effectively in diverse teams to achieve shared goals.</p>
+                        <h3>Collaborative Team Player</h3>
+                        <p>Effective communicator and team player, experienced in collaborating with cross-functional teams to deliver high-quality software solutions. Proven track record of contributing to projects through active collaboration, adaptability, and a commitment to achieving shared objectives.</p>
                     </div>
                 </li>
             </ul>
