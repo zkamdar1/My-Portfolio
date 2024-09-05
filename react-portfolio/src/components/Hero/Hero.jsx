b6ae8180c12566318afcd9c358007f56310f1cb8
zkamdar <zkamdar0908@gmail.com>
2024-09-05 17:06:06 -0400

    add resume to portfolio

--- react-portfolio/src/components/Hero/Hero.jsx
+++ react-portfolio/src/components/Hero/Hero.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { getImageUrl } from '../../utils';
 import styles from './Hero.module.css';
 
+const path = './ZaidKamdar_resume.pdf';
 export const Hero = () => {
   return (
     <section className={styles.container}>
@@ -11,6 +12,7 @@ export const Hero = () => {
             I'm currently a Computer Science student at the University of North Carolina at Chapel Hill (Go Heels!!!). Passionate about technology and innovation, I'm always eager to learn and take on new challenges. Feel free to reach out if you'd like to connect!
             </p>
             <a href="mailto:zkamdar0908@gmail.com" className={styles.contactBtn}>Contact Me</a>
+            <a href={path} download="ZK_resume.pdf" className={styles.resumeBtn}>My Resume</a>
         </div>
         <img 
             src={getImageUrl("hero/heroImage.png")} 
